b26e0fb622f77e340ab74e153b4907d84416d225
Matej Posavac <matejposavac27@gmail.com>
2022-05-02 12:36:48 +0200

    Refactored RoomsForm, added SearchForm

--- src/components/Rooms/RoomsPresenter/RoomsForm/RoomsForm.tsx
+++ src/components/Rooms/RoomsPresenter/RoomsForm/RoomsForm.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useTranslation } from "../../../../context";
 import { InputSelect } from "../../../shared/InputSelect/InputSelectContatiner/InputSelectContainer";
+import SearchForm from "../../../shared/SearchForm/SearchForm";
 import { RoomsFormPresenterProps } from "../types";
 
 export default function RoomsFormPresenter({
@@ -14,40 +15,41 @@ export default function RoomsFormPresenter({
 }: RoomsFormPresenterProps) {
   const { t } = useTranslation();
   return (
-    <div className="rooms__form__container__content">
-      <h3>{t("form.rooms")}</h3>
-      <div className="rooms__form__container__content__field">
-        <label>{t("form.date")}</label>
-        <input
-          type="date"
-          name="date"
-          value={values.date}
-          onChange={(e: any) => handleFormChange(e)}
-        />
-      </div>
-      <div className="rooms__form__container__content__field">
-        <label>{t("form.building")}</label>
-        <InputSelect
-          name="building"
-          selectedOption={values.building}
-          optionsList={buildingOptions}
-          onChange={handleFormChange}
-        />
-      </div>
-      <div className="rooms__form__container__content__field">
-        <label>{t("form.room")}</label>
-        <InputSelect
-          name="room"
-          selectedOption={values.room}
-          optionsList={roomsOptions}
-          onChange={handleFormChange}
-          disabled={!values.building.value}
-        />
-      </div>
-      <button onClick={handleSearch}>{t("search")}</button>
-      {isThereCalendarEvents && (
-        <button onClick={toggleStatsOverlay}>{t("button.statistics")}</button>
-      )}
-    </div>
+    <SearchForm
+      searchText={"form.rooms"}
+      date={values.date}
+      handleFormChange={handleFormChange}
+      handleSearch={handleSearch}
+      additionalInputFields={
+        <>
+          <div className="rooms__form__container__content__field">
+            <label>{t("form.building")}</label>
+            <InputSelect
+              name="building"
+              selectedOption={values.building}
+              optionsList={buildingOptions}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="rooms__form__container__content__field">
+            <label>{t("form.room")}</label>
+            <InputSelect
+              name="room"
+              selectedOption={values.room}
+              optionsList={roomsOptions}
+              onChange={handleFormChange}
+              disabled={!values.building.value}
+            />
+          </div>
+        </>
+      }
+      additionalButtons={
+        isThereCalendarEvents ? (
+          <button onClick={toggleStatsOverlay}>{t("button.statistics")}</button>
+        ) : (
+          <></>
+        )
+      }
+    />
   );
 }
