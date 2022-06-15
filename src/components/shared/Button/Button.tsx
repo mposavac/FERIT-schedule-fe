8997251a07cb71f5640f055c3b50df121cbd30ab
Matej Posavac <matejposavac27@gmail.com>
2022-06-15 13:52:52 +0200

    Added InputSelect outside click support

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useTranslation } from "../../../context";
 import "./Button.scss";
+import { ButtonProps } from "./types";
 
-export default function Button({ text, icon, onClick }: any) {
+export default function Button({ text, icon, onClick }: ButtonProps) {
   const { t } = useTranslation();
   return (
     <div className="button" onClick={onClick}>
